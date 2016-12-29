9b7d4c8cadae56439e02e3ee31b5d0d279de588e
jeezhau <zhihuang_2009@163.com>
2016-12-29 18:12:15 +0800

    基本的消息接收测试

--- m_leyi/src/main/java/me/jeekhan/leyi/controller/WechatAction.java
+++ m_leyi/src/main/java/me/jeekhan/leyi/controller/WechatAction.java
@@ -1,9 +1,20 @@
 package me.jeekhan.leyi.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import me.jeekhan.leyi.common.SunSHAUtils;
+import me.jeekhan.leyi.wxapi.CommonAPI;
 /**
  * 微信
  * @author Jee Khan
@@ -34,7 +46,7 @@ public class WechatAction {
 	@ResponseBody
 	public String virifyWXServer(@RequestParam("signature")String signature,@RequestParam("timestamp")String timestamp,
 			@RequestParam("nonce")String nonce, @RequestParam("echostr")String echostr){
-		String token = "";
+		String token = CommonAPI.getParam("TOKEN");
 		String[] arr = {token,timestamp,nonce};
 		Arrays.sort(arr);
 		String strAll = arr[0] + arr[1] + arr[2];
@@ -44,16 +56,44 @@ public class WechatAction {
 		} catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
-		if(signature == ret){
+		if(signature.equals(ret)){
 			return echostr;
 		}
 		return "fail";
 	}
 	
-	@RequestMapping(value="/hl",method=RequestMethod.GET)
+	@SuppressWarnings("rawtypes")
+	@RequestMapping(value="/wx",method=RequestMethod.POST)
 	@ResponseBody
-	public String test(){
-		return "ok";
+	public String recvMsg(InputStream is){
+		BufferedReader br;
+		StringBuffer sb = new StringBuffer();
+		Map<String,String> map = new HashMap<>();
+		try {
+			String line = "";
+			br = new BufferedReader(new InputStreamReader(is,"utf-8"));
+			while((line=br.readLine())!=null){
+				sb.append(line);
+			}
+			Document doc = DocumentHelper.parseText(sb.toString());
+			Element xml = doc.getRootElement();
+			/*  URL:http://m.jeekhan.me/leyi4m/wx
+				ToUserName:jee_khan_zhao
+				FromUserName:jee_khan_zhao
+				CreateTime:1231313
+				MsgType:text
+				Content:这个一个测试
+				MsgId:5643535434535*/
+            for (Iterator i = xml.elementIterator(); i.hasNext();) {
+                Element node = (Element) i.next();
+                map.put(node.getName(), node.getText());
+                System.out.println(node.getName() + ":" + node.getText());
+            }
+
+		} catch (IOException | DocumentException e) {
+			e.printStackTrace();
+		}
+		return "success";
 	}
 
 }
